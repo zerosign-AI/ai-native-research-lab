3d865a41f9167d47a2ec1330d5a4a809d79f506a
zerosign-AI <mozerosign@gmail.com>
2026-06-30 17:44:29 +0900

    Add files via upload

---
+++ docs/config.js
@@ -0,0 +1,3 @@
+// Apps Script Web App URL을 여기에 넣으세요.
+// 예: const API_URL = 'https://script.google.com/macros/s/AKfycb.../exec';
+const API_URL = 'https://script.google.com/a/macros/dktechin.com/s/AKfycbwn42R5atXjdwvL_V1RqOy9PHp8FU1aFFoAttoE4WzPVTAzcD9C3B5udcKQXKsRAQZ2/exec';
