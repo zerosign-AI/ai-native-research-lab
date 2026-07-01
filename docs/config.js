ec5a59cfdaecb978022dadab7fc6c9e5e39dfa94
zerosign-AI <mozerosign@gmail.com>
2026-07-01 13:43:58 +0900

    Add files via upload

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 window.ANP_CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbwxr-pkGaXoFabX4QgyiJivLECRDVMzCyiQQPa1XoSIibBtjnDpRQ50FZiUpY5thmL7/exec',
-  APP_VERSION: 'v1.0.0-rc2'
+  APP_VERSION: 'v1.0.0-rc3'
 };
